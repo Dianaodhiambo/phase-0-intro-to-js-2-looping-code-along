f9e0fb9b7beecc777121a44fc6128591417ec0f0
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-08-13 17:45:38 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+return messages;
+  }
+console.log (writeCards(["Ada", "Brendan", "Ali"], "birthday"))
+
+
+function countDown(){
+    var countDown = 10;
+    for(;countDown > 0; countDown--) { 
+        console.log(countDown);
+    }
+    console.log(countDown);
+}
+    
